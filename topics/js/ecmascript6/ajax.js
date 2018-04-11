ff6519f74413882a11c19c821d776671ee4f0f4e
jsnrice <jsnrice@gmail.com>
2018-04-11 09:40:08 -0600

    Refactored node-fetch by applying the timeout option already provided.

--- topics/js/ecmascript6/ajax.js
+++ topics/js/ecmascript6/ajax.js
@@ -2,80 +2,75 @@
  * Asynchronous functions with async and await.
  ***********************************************/
 "use strict";
-const fetch = require('node-fetch')
-let copyOfUrl = "https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json";
+const fetch = require('node-fetch');
+// Lots of data (130Mb):
+let TARGET_URL = "https://raw.githubusercontent.com/zemirco/sf-city-lots-json/master/citylots.json";
 
 class AjaxService {
     constructor() {
-        this.url = copyOfUrl;
-        this._timeout = {active: false, seconds: 0};
-    }
-
-    // TODO: combine a setTimeout with fetch
-    fetchTimeout(url, options) {
-        if (this._timeout.active && this._timeout.seconds > 0) {
-            let timeoutError = {
-                ok: false,
-                status: 408
-            };
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    // implement...
-                }, this._timeout.seconds * 1000);
-            })
-        }
-    }
-
-    resetUrl() {
-        this.url = copyOfUrl;
+        this._timeout = {active: false, ms: 0};
     }
 
     set timeout(timeout) {
 	    this._timeout = JSON.parse(JSON.stringify(timeout)); // deep copy
     }
 
-    async asyncFetchImpl() {
-        let response = await fetch(this.url);
-        let data = null;
+    resetTimeout() {
+        this.timeout = {active: false, ms: 0};
+    }
+
+    /***
+     * Asynchronous fetch decorator.
+     *
+     * @param url to target
+     * @param options should
+     * @returns {Promise.<*>}
+     */
+    async asyncFetch(url, options = {
+        cache: 'default',
+        credentials: 'omit',
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'GET',
+        mode: 'same-origin',
+        redirect: 'manual',
+        referrer: 'client',
+        timeout: 0
+    }) {
+        if (this._timeout.active && this._timeout.ms > 0) {
+            options.timeout = this._timeout.ms;
+            let msg = (options.timeout >= 1000) ? `${options.timeout/1000} second(s)` : `${options.timeout} millisecond(s)`
+            console.log(`timeout set to ${msg}`);
+        }
+
+        let response = await fetch(url, options);
+
         if (response.ok) {
     	    return await response.json();
         } else {
-    	    throw new Error(response.status);
-        }	
-    };
-
-    asyncFetchData() {
-        if (this._timeout.active && this._timeout.seconds > 0) {
-            setTimeout(() => {
-                return this.asyncFetchImpl();
-            }, this._timeout.seconds * 1000);
-        } else {
-            return this.asyncFetchImpl();
+            throw new Error(response.status);
         }
     };
 
-
     // Run the data fetch function.
-    run() {
-        this.asyncFetchData()
-   	        .then(data => console.log("Data type: ", data.type))
+    fetch(url, options) {
+        this.asyncFetch(url, options)
+   	        .then(data => console.log("Acquired fetched data: ", data.type))
 		    .catch(reason => console.error("Caught error: ", reason.message));
     };
 }
 
- let ajaxService = new AjaxService();
+let ajaxService = new AjaxService();
 
 // Demo the service:
-ajaxService.run();
+ajaxService.fetch(TARGET_URL);
 
-// Now test with an invalid url to see what happens:
-ajaxService.url = "bogus";
+// This should run first since it's a bogus url and the response is faster:
+ajaxService.fetch(TARGET_URL + "bogus");
 
-// This should run first since it's a bogus url:
-ajaxService.run();
+// Test the timeout of the initiating request by making a very fast request that should timeout:
+ajaxService.timeout = {active: true, ms: 1000};
+ajaxService.fetch(TARGET_URL);
 
-ajaxService.resetUrl();
-
-// ajaxService.timeout = {active: true, seconds: 1};
-//
-// ajaxService.run();
+ajaxService.resetTimeout();
